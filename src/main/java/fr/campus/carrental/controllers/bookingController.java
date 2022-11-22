2a2a4e926d02fdda83e0c750f327662ab032de46
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-11-22 14:07:09 +0100

    feat: Post, Put & Delete methods

--- src/main/java/fr/campus/carrental/controllers/bookingController.java
+++ src/main/java/fr/campus/carrental/controllers/bookingController.java
@@ -17,15 +17,19 @@ public class bookingController {
         this.bookingDao = bookingDao;
     }
 
+    // Lister toutes les réservations
     @GetMapping("/bookings")
     public List<Booking> GetAllBookings(){
         return bookingDao.findAll();
     }
+
+    // Consulter une réservation si elle existe
     @GetMapping("/booking/{id}")
     public Optional<Booking> bookingDetail(@PathVariable int id){
         return bookingDao.findById(id);
     }
 
+    // Enregistrer une réservation si elle existe
     @PostMapping(value = "/bookings")
     // @RequestBody demande à Spring de convertir le corps de la requête HTTP en JSON
     // La requête en JSON sera convertie en objet Car
@@ -33,14 +37,32 @@ public class bookingController {
         bookingDao.save(newBooking);
     }
 
+    // Lister les réservations par client
     @GetMapping("/bookings/user/{userId}")
     public List<Booking> listByUserId(@PathVariable int userId){
         return bookingDao.findByUserId(userId);
     }
+
+    // Lister les réservations par véhicule
     @GetMapping("/bookings/vehicle/{vehicleId}")
     public List<Booking> listByVehicleId(@PathVariable int vehicleId){
         return bookingDao.findByVehicleId(vehicleId);
     }
+
+    @PutMapping("/booking/update")
+    // l'id sera dans le corps de la requête.
+    // avec save(), le dao recherchera l'instance à l'id indiqué et le remplacera par la nouvelle instance
+    public void updateBooking(@RequestBody Booking newBooking){
+        bookingDao.save(newBooking);
+    }
+
+    @DeleteMapping("booking/delete/{bookingId}")
+    public void deleteBooking(@PathVariable int bookingId){
+        Optional<Booking> target = bookingDao.findById(bookingId);
+        target.ifPresent(bookingDao::delete);
+    }
+
+    // Lister les réservations par interval de dates
 //    @GetMapping("/bookings/dateinterval{startDate}-{EndDate}")
 //    public List<Booking> findByDateInterval(@PathVariable Date startDate, @PathVariable Date EndDate){
 //        return bookingDao.findByDateInterval(startDate, EndDate);
--- src/main/java/fr/campus/carrental/dao/BookingDao.java
+++ src/main/java/fr/campus/carrental/dao/BookingDao.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Date;
 import java.util.List;
